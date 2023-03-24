c96421807aed8ca59f654c1dc31bbe66663e9cc8
Johannes Pesenhofer <johannes.pesenhofer@student.tugraz.at>
2023-03-24 19:21:45 +0100

    WIP: added imsdb scraper

---
+++ src/scraping/imsdb.py
@@ -0,0 +1,68 @@
+import re
+import urllib.request
+from typing import List
+from bs4 import BeautifulSoup
+
+from scrapy.crawler import CrawlerProcess
+from w3lib.url import url_query_cleaner
+from scrapy.spiders.crawl import CrawlSpider, LinkExtractor, Rule
+
+
+def process_links(links):
+    for link in links:
+        link.url = url_query_cleaner(link.url)
+        yield link
+
+
+class ImsdbSpider(CrawlSpider):
+    name = "imsdb"
+    base_url = "https://imsdb.com"
+    allowed_domains = ["imsdb.com"]
+    start_urls = ["https://imsdb.com/all-scripts.html"]
+    rules = (
+        Rule(
+            LinkExtractor(
+                allow=re.escape("https://imsdb.com/Movie%20Scripts/")
+            ),
+            callback="parse_item",
+        ),
+    )
+    custom_settings = {
+        "ROBOTSTXT_OBEY": False,
+    }
+
+    def _get_genres_from_links(self, links) -> List[str]:
+        genres = []
+        for link in links:
+            ref = link.get("href")
+            if "genre" in ref:
+                genres.append(link.text)
+        return genres
+
+    def _get_script_from_links(self, links):
+        script_link = None
+        for link in links:
+            if "Read" in link.text and "Script" in link.text:
+                script_link = self.base_url + link.get("href")
+        response = urllib.request.urlopen(script_link)
+        soup = BeautifulSoup(response, "html.parser")
+        return soup.find("td", {"class": "scrtext"}).find("pre").text
+
+    def parse_item(self, response):
+        soup = BeautifulSoup(response.text)
+        imdbs_box = soup.find(text="Genres").parent.parent
+        links = imdbs_box.find_all("a")
+        genres = self._get_genres_from_links(links)
+        script = self._get_script_from_links(links)
+        # TODO SAVE FILE IN SOME FORMAT
+
+
+def main():
+    process = CrawlerProcess()
+    process.crawl(ImsdbSpider)
+    process.start()
+    pass
+
+
+if __name__ == "__main__":
+    main()
