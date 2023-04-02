b1324907fd39e629f3546cd45c692be22ad389e2
Johannes Pesenhofer <johannes.pesenohfer@student.tugraz.at>
2023-04-02 22:52:47 +0200

    added new steps to main

--- src/main.py
+++ src/main.py
@@ -1,4 +1,29 @@
-import re
+from typing import List
+import pandas as pd
+from src.dataset_preparaion.pipeline import MovieScriptPreprocessor, SampleT
+from src.scraping.imsdb import ImsdbScraper
+from src.scraping.dailyscript import DailyscriptScraper
 
 
-print(re.sub(r"\.\s+[A-Z]", lambda x: x.group().lower(), "Come and join us. She smiles a crazy smile. Reaches out for Quaid. A SNAKE appears from around the back of her"))
+if __name__ == "__main__":
+    scrapers = [
+        ImsdbScraper(),
+        DailyscriptScraper(),
+        # Add more if needed
+    ]
+
+    # Run Scrapers
+    for scraper in scrapers:
+        scraper.run()
+
+    # Apply preprocessing and merge datasources
+    preprocessor = MovieScriptPreprocessor("./data/preprocessed_data.json")
+    for scraper in scrapers:
+        preprocessor.process_file(scraper.out_file)
+        # For samples use:
+        # preprocessor.process_sample(a_new_entry)
+
+    # TODO: Continue here:
+    # SampleT is just a type that somehow represents our samples (a dict)
+    data: List[SampleT] = preprocessor.load_output()
+    print(pd.DataFrame.from_records(data).head())
